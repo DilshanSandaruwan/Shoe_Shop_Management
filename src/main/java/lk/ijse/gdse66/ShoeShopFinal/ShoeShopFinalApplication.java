d4599c2e6eb9f77a31c1346809f39cbe5fa489c0
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-05-30 17:37:02 +0530

    add mevan dependancies

--- src/main/java/lk/ijse/gdse66/ShoeShopFinal/ShoeShopFinalApplication.java
+++ src/main/java/lk/ijse/gdse66/ShoeShopFinal/ShoeShopFinalApplication.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.ShoeShopFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShoeShopFinalApplication {
