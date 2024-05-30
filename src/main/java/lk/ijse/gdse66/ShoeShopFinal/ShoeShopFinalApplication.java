93eb2aa457e05417773c59fe60c237554122ac94
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-05-30 17:29:32 +0530

    update project

---
+++ src/main/java/lk/ijse/gdse66/ShoeShopFinal/ShoeShopFinalApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.ShoeShopFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeShopFinalApplication.class, args);
+	}
+
+}
