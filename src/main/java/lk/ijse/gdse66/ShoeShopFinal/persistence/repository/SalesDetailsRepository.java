b888cba9d49a478cf56f6640836c423315e84889
Dilshan Sandaruwan <129358842+DilshanSandaruwan@users.noreply.github.com>
2024-05-30 18:51:53 +0530

    create sales entity and repository

---
+++ src/main/java/lk/ijse/gdse66/ShoeShopFinal/persistence/repository/SalesDetailsRepository.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse66.ShoeShopFinal.persistence.repository;
+
+import lk.ijse.finalcoursework.shoeshop.persistence.entity.SalesDetails;
+import lk.ijse.gdse66.ShoeShopFinal.persistence.entity.SalesDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author: Dilshan Sandaruwan,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+public interface SalesDetailsRepository extends JpaRepository<SalesDetails,String> {
+    Boolean existsBySalesOrderNo(String id);
+    List<SalesDetails> findAllBySalesOrderNo(String id);
+    void deleteAllBySalesOrderNo(String id);
+    Boolean existsAllBySalesOrderNo(String id);
+}
